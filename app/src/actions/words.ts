48fdb7838de0461b52bc55c711900e5c3e69c579
Adam Cardenas <cevaris@gmail.com>
2020-10-21 03:17:00 +0000

    working words app

--- app/src/actions/words.ts
+++ app/src/actions/words.ts
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-interface Word {
+export interface WordsResponse {
+    ok: boolean
+    value: Word[]
+}
+
+export interface Word {
     value: string
     definition: string
 }
 
 export async function getWords(query: string): Promise<Word[]> {
     const wordsRequestURL = `http://localhost:3000/words.json?q=${query}`;
-    const response = await axios.get<Word[]>(wordsRequestURL);
-    return response.data;
+    const response = await axios.get<WordsResponse>(wordsRequestURL);
+    return response.data.value;
 }
--- app/src/components/WordSearchContainer.tsx
+++ app/src/components/WordSearchContainer.tsx
@@ -1,22 +1,32 @@
-import { IonSearchbar } from '@ionic/react';
-import React from 'react';
-import { getWords } from '../actions/words';
+import { IonItem, IonLabel, IonList, IonSearchbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { getWords, Word } from '../actions/words';
 
 interface ContainerProps { }
 
 const WordSearchContainer: React.FC<ContainerProps> = () => {
+  const [words, setWords] = useState<Word[]>([]);
 
   const setSearchText = async (query: string | undefined) => {
     if (query) {
-      console.log('setSearchText', query);
-      const results = await getWords(query);
-      console.log(results);
+      const results: any = await getWords(query);
+      setWords(results);
     }
   }
 
   return (
     <div>
-      <IonSearchbar onIonChange={e => setSearchText(e.detail.value)}></IonSearchbar>
+      <IonSearchbar onIonChange={e => setSearchText(e.detail.value)} debounce={500}></IonSearchbar>
+      <IonList>
+        {words && words.map((word, idx) => (
+          <IonItem key={idx}>
+            <IonLabel>
+              <h1>{word.value}</h1>
+              <div>{word.definition}</div>
+            </IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
     </div>
   );
 };
